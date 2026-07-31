6d7afb2ae27b3ecb1f5bbde27365270cee52d30b
Swati Ranyal <121052889+swatiranyalsharma@users.noreply.github.com>
2026-07-31 17:29:23 +0530

    Time: 2 ms (62.06%), Space: 46.1 MB (78.82%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int[] prefix = new int[1001];
         for(int i=0;i<trips.length;i++){
-            int val = trips[i][0];
-            int s = trips[i][1];
-            int e = trips[i][2];
-            prefix[s] = prefix[s]  + val;
-            prefix[e] = prefix[e] - val;
+            int c = trips[i][0];
+            int p = trips[i][1];
+            int d = trips[i][2];
+            prefix[p] = prefix[p] +c;
+            prefix[d] = prefix[d] - c;
         }
-        for(int i=0;i<prefix.length;i++){
+        for(int i=0; i<prefix.length;i++){
             if(i!=0) prefix[i] = prefix[i-1] + prefix[i];
             if(prefix[i] > capacity) return false;
         }
